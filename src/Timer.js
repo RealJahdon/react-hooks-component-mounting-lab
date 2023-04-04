b14bfbeab0f1dbb27bd21415b1cc04a2aa05f082
Jahdon Ervin <Jahdonervin@gmail.com>
2023-04-03 20:00:08 -0400

    commit tree 31f41fd3da024ca834787ba5d9acd45bcbfdee55

--- src/Timer.js
+++ src/Timer.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 
+
 class Timer extends Component {
   state = {
     time: 0,
     color: "#" + Math.floor(Math.random() * 16777215).toString(16)
   };
 
-  // add your code here
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)    
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)    
+  }
 
   render() {
     const { time, color } = this.state;
@@ -19,19 +26,16 @@ class Timer extends Component {
       </section>
     );
   }
-
-  //clock functions
+  
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
     }));
   };
-
   stopClock = () => {
     clearInterval(this.interval);
   };
-
-  // for the 'x' button,
+ 
   handleClose = () => {
     this.props.removeTimer(this.props.id);
   };
